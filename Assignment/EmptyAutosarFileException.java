4793b3a53311fc100d56f801b73421346288f6eb
HanaSalah10 <110139751+HanaSalah10@users.noreply.github.com>
2023-04-19 18:04:49 +0200

    Delete Assignment directory

--- Assignment/EmptyAutosarFileException.java
+++
@@ -1,8 +0,0 @@
-
-public class EmptyAutosarFileException extends RuntimeException{
-	EmptyAutosarFileException(String message)
-	{
-		super(message);
-	}
-
-}
--- Assignment/testcases.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-javac lab_06.java
-java lab_06 NormalCase.arxml
-echo.
-java lab_06 ExtensionTest.html
-echo.
-java lab_06 Empty.arxml
-pause
